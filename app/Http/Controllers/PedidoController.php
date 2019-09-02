214680da3fa0718bb681334b60d9c3bd45563706
Renato Mosca <rmosca2001@hotmail.com>
2019-09-02 17:23:23 -0300

    implementação rotina de criação de usuário no Moodle

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -7,6 +7,7 @@ use App\Pedido;
 // use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Client;
 
 
 class PedidoController extends Controller
@@ -20,11 +21,32 @@ class PedidoController extends Controller
             'nr_parcelas' => 0,
         ]);
 
-        // $itens = Itens_Pedido::create([
-        //     'id_pedido' => $pedidos->id,
-        //     'id_produto' => $data->id_produto,
-        //     'valor' => $data->valor,
-        // ]);
+        //criar usuario no Moodle
+        $client = new Client();
+        $response = $client->request('POST', "reeduca.amtsol.com.br/moodle/_services/response.php", [
+            'form_params' => [
+                'action' => 'create_user',
+                'username' => auth()->user()->email,
+                'first_name' => auth()->user()->name,
+                'last_name' => '.',
+                'email' => auth()->user()->email,
+                // 'password' => $user->moodle_password,
+                'password' => 'Reeduca@2019',
+                'city' => auth()->user()->cidade,
+            ]
+        ]);
+
+        //matricular no curso
+        $client = new Client();
+        $response = $client->request('POST', "reeduca.amtsol.com.br/moodle/_services/response.php", [
+            'form_params' => [
+                'action' => 'create_enrollment',
+                'moodle_course_id' => 1,
+                'moodle_user_id' => $data->id_usuario,
+            ]
+        ]);
+
+
         return view('pedido',['pedido'=>$pedido]);
 
         }
